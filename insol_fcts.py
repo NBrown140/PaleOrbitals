830abb0ecbd5f2d9d0bca149ca08bbcb2ee2bb75
Nicolas <nic.brown0@gmail.com>
2015-02-05 22:29:53 -0500

    Initial insolation functions
    Started dcostring and monthly insolation function

---
+++ insol_fcts.py
@@ -0,0 +1,42 @@
+
+"""
+Functions for the computation of insolation
+
+Code is inspired from Laskar 2004 fortran code, which can be found here:
+http://www.imcce.fr/Equipes/ASD/insola/earth/La2004/insolsub.f
+
+Modified by Nicolas Brown (02/2015)
+
+Modifications:
+
+"""
+
+
+
+
+def wmcal(month,e,eps,pibarh,phi,w)
+	"""
+	Monthly insolation for a given latitude 
+	
+	Dependencies:
+		trueavg()
+		
+	
+	IN:
+		month: 
+		e,eps,pibarh: orbital elements for date tps
+			e: eccentricity
+			eps: obliquity
+			pibarh: longitude of perihelion from equinox at given date
+		phi: latitude of point on Earth
+		
+	OUT:
+		w: monthly insolation at given latitude
+
+	Last Modified:
+		Nicolas Brown (02/2015)
+
+	"""
+	
+	
+	pibar = pibarh + pi

b7648715616d300fe2c0c41e5b08fb14a78bd80b
Nicolas <nic.brown0@gmail.com>
2015-02-07 00:21:05 -0500

    Added daily insolation function: cwj
    Also:
    Converted all fat inputs to floats
    Added to docstring comment about changing names of functions from
    french to english

--- insol_fcts.py
+++ insol_fcts.py
@@ -5,6 +5,9 @@ Functions for the computation of insolation
 Code is inspired from Laskar 2004 fortran code, which can be found here:
 http://www.imcce.fr/Equipes/ASD/insola/earth/La2004/insolsub.f
 
+Kept same function(subroutine in fortran) names except for vraimoy and moyvrai,
+which became true_avg and avg_true, respectively.
+
 Modified by Nicolas Brown (02/2015)
 
 Modifications:
@@ -19,15 +22,19 @@ def true_avg(hl,e,pibar):
 	and longitude of perihelion(pibar).
 	
 	IN:
-		hl: true longitude
-		e: eccentricity
-		pibar: longitude of perihelion
+		hl: true longitude....................................... float or int
+		e: eccentricity.......................................... float or int
+		pibar: longitude of perihelion........................... float or int
 		
 	OUT:
-		hlm: average longitude
+		hlm: average longitude................................... float
 	"""
+	# Convert input to floats if not already done
+	hl,e,pibar = float(hl),float(e),float(pibar)
+	
 	eV = hl-pibar
-	return hl-2*e*m.sin(eV)+(3*e**2/4+e**4/8)*m.sin(2*eV)-(e**3/3+e**5/8)*m.sin(3*eV)+5*e**4/32*m.sin(4*eV)-3*e**5/40*m.sin(5*eV)
+	return hl-2*e*m.sin(eV)+(3*e**2/4+e**4/8)*m.sin(2*eV)-(e**3/3+e**5/8)*m.sin(3*eV) \
+	+5*e**4/32*m.sin(4*eV)-3*e**5/40*m.sin(5*eV)
 
 
 def avg_true(hlm,e,pibar):
@@ -36,18 +43,77 @@ def avg_true(hlm,e,pibar):
 	and longitude of perihelion(pibar).
 	
 	IN:
-		hlm: average longitude
-		e: eccentricity
-		pibar: longitude of perihelion
+		hlm: average longitude................................... float or int
+		e: eccentricity.......................................... float or int
+		pibar: longitude of perihelion........................... float or int
 		
 	OUT:
-		hl: true longitude
+		hl: true longitude....................................... float
 	"""
+	# Convert input to floats if not already done
+	hlm,e,pibar = float(hlm),float(e),float(pibar)
+	
 	eM = hlm-pibar
-	return hlm+(2*e-e**3/4 + 5*e**5/96)*m.sin(eM)+(5*e**2/4 - 11*e**4/24)*m.sin(2*eM)+(13*e**3/12 - 43*e**5/64)*m.sin(3*eM)+ 103*e**4/96*m.sin(4*eM) + 1097*e**5/960*m.sin(5*eM)
+	return hlm+(2*e-e**3/4 + 5*e**5/96)*m.sin(eM)+(5*e**2/4 - 11*e**4/24)*m.sin(2*eM) \
+	+(13*e**3/12 - 43*e**5/64)*m.sin(3*eM)+ 103*e**4/96*m.sin(4*eM) \
+	+1097*e**5/960*m.sin(5*eM)
 
 
-def wmcal(month,e,eps,pibarh,phi,w):
+def cwj(So,wd,e,pibar,eps,phi):
+	"""
+	Daily insolation for a given latitude
+	
+	IN:
+		So: solar constant....................................... float or int
+		wd: true longitude of sun (in radians) from
+		    equinox at date...................................... float or int
+		e: eccentricity.......................................... float or int
+		pibar: longitude of perihelion from equinox
+		       at date + pi...................................... float or int
+		eps: obliquity........................................... float or int
+		phi: latitude on Earth................................... float or int
+		
+	OUT:
+		w: daily insolation...................................... float
+	"""
+	# Convert input to floats if not already done
+	wd,e,pibar,eps,phi = float(wd),float(e),float(pibar),float(eps),float(phi)
+	
+	# True anomaly
+	v = wd - pibar
+	
+	# Earth-Sun distance
+	rho = (1-e**2) / (1+e*m.cos(v))
+	
+	# Sun declination
+	delta = m.asin(m.sin(eps)*m.sin(wd))
+	
+	# Latitude can either have: (1) sunrise and sunset, (2) no sunset or
+	# (3) no sunrise  
+	aux = m.pi/2. - abs(delta)
+	a1 = m.pi/2. - delta
+	a2 = m.pi/2. + delta
+	
+	## (1) Sunrise and sunset
+	if (-1*aux < phi) and (phi < aux):
+		# Hour angle of sunrise and sunset
+		ho = m.acos(-1*m.tan(phi) * m.tan(delta))
+		# Insolation
+		return (ho*m.sin(phi)*m.sin(delta) + m.cos(phi)*m.cos(delta)*m.sin(ho)) \
+		* So/(m.pi*rho**2)
+		
+	## (2) No sunset
+	elif (phi > a1) or (phi < -1*a2):
+		return So * m.sin(phi)*m.sin(delta) / rho**2
+		
+	## (3) No sunrise
+	elif (phi < -1*a1) or (phi > a2):
+		return 0.
+	
+	print 'Should never get here! if-statements should contain all possibilities'
+	
+
+def wmcal(month,e,eps,pibarh,phi):
 	"""
 	Monthly insolation for a given latitude 
 	
